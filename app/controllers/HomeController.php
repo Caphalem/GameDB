cc82ff03974a6531e9872a5c51cb169ac428a56e
Caphalem <Erik.Volkovinskis@gmail.com>
2014-12-16 02:10:03 +0200

    Updating the search engine
    Fully functional and more interactive.

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -10,22 +10,24 @@ class HomeController extends BaseController {
 
         $query=Input::get('query');
         $games = Game::where('title', 'LIKE', '%'.$query.'%')->get();
-        $t = 0;
-        $r = 0;
-        $p = 0;
+        $t = 2;
+        $r = 2;
+        $p = 2;
+        $d = 2;
 
         return View::make('results')->with('games', $games)->with('query', $query)->with('t', $t)->with('r', $r)
-            ->with('p', $p);
+            ->with('p', $p)->with('d', $d);
     }
 
     public function orderByTitle()
 
     {
-        $r = 0;
-        $p = 0;
+        $r = 2;
+        $p = 2;
         $t = Input::get('t');
+        $d = 2;
         $query=Input::get('query');
-        if ($t == 0) {
+        if ($t <> 1) {
             $games = Game::where('title', 'LIKE', '%' . $query . '%')->orderBy('title', 'ASC')->get();
             $t = 1;
         }
@@ -35,18 +37,18 @@ class HomeController extends BaseController {
         }
 
         return View::make('results')->with('games', $games)->with('query', $query)->with('t', $t)->with('r', $r)
-            ->with('p', $p);
+            ->with('p', $p)->with('d', $d);
     }
 
     public function orderByRelease()
 
     {
-        $t = 0;
+        $t = 2;
         $r = Input::get('r');
-        $p = 0;
-
+        $p = 2;
+        $d = 2;
         $query=Input::get('query');
-        if ($r == 0) {
+        if ($r <> 1) {
             $games = Game::where('title', 'LIKE', '%' . $query . '%')->orderBy('release_date', 'ASC')->get();
             $r = 1;
         }
@@ -56,28 +58,57 @@ class HomeController extends BaseController {
         }
 
         return View::make('results')->with('games', $games)->with('query', $query)->with('t', $t)->with('r', $r)
-            ->with('p', $p);
+            ->with('p', $p)->with('d', $d);
     }
 
-//    public function orderByPublisher()
-//
-//    {
-//        $query=Input::get('query');
-//        $t = 0;
-//        $r = 0;
-//        $p = Input::get('p');
-//        if ($p == 0) {
-//            $games = Game::where('games.title', 'LIKE', '%' . $query . '%')->join('publisher', 'games.publisher_id', '=', 'publisher.id')
-//            ->orderBy('publisher.title', 'AS', 'publisher_title', 'ASC')->get();
-//            $p = 1;
-//        }
-//        else {
-//            $games = Game::where('games.title', 'LIKE', '%' . $query . '%')->join('publisher', 'games.publisher_id', '=', 'publisher.id')
-//                ->orderBy('publisher.title', 'AS', 'publisher_title', 'DESC')->get();
-//            $p = 0;
-//        }
-//
-//        return View::make('results')->with('games', $games)->with('query', $query)->with('t', $t)->with('r', $r)
-//            ->with('p', $p);
-//    }
+    public function orderByPublisher()
+
+    {
+        $query=Input::get('query');
+        $t = 2;
+        $r = 2;
+        $p = Input::get('p');
+        $d = 2;
+        if ($p <> 1) {
+            $games = Game::select('games.title', 'games.publisher_id', 'games.developer_id', 'games.release_date', 'publisher.id', 'publisher.title AS pub_title')
+                ->where('games.title', 'LIKE', '%' . $query . '%')->join('publisher', 'games.publisher_id', '=', 'publisher.id')
+            ->orderBy('pub_title', 'ASC')->get();
+            $p = 1;
+        }
+        else {
+            $games = Game::select('games.title', 'games.publisher_id', 'games.developer_id', 'games.release_date', 'publisher.id', 'publisher.title AS pub_title')
+                ->where('games.title', 'LIKE', '%' . $query . '%')->join('publisher', 'games.publisher_id', '=', 'publisher.id')
+                ->orderBy('pub_title', 'DESC')->get();
+            $p = 0;
+        }
+
+        return View::make('results')->with('games', $games)->with('query', $query)->with('t', $t)->with('r', $r)
+            ->with('p', $p)->with('d', $d);
+    }
+
+    public function orderByDeveloper()
+
+    {
+        $query=Input::get('query');
+        $t = 2;
+        $r = 2;
+        $p = 2;
+        $d = Input::get('d');
+        if ($d <> 1) {
+            $games = Game::select('games.title', 'games.publisher_id', 'games.developer_id', 'games.release_date', 'developers.id', 'developers.title AS dev_title')
+                ->where('games.title', 'LIKE', '%' . $query . '%')->join('developers', 'games.developer_id', '=', 'developers.id')
+                ->orderBy('dev_title', 'ASC')->get();
+            $d = 1;
+        }
+        else {
+            $games = Game::select('games.title', 'games.publisher_id', 'games.developer_id', 'games.release_date', 'developers.id', 'developers.title AS dev_title')
+                ->where('games.title', 'LIKE', '%' . $query . '%')->join('developers', 'games.developer_id', '=', 'developers.id')
+                ->orderBy('dev_title', 'DESC')->get();
+            $d = 0;
+        }
+
+        return View::make('results')->with('games', $games)->with('query', $query)->with('t', $t)->with('r', $r)
+            ->with('p', $p)->with('d', $d);
+    }
+
 }
--- app/views/results.blade.php
+++ app/views/results.blade.php
@@ -14,29 +14,71 @@
             {{ Form::open(array('route' => 'byTitle')) }}
             <p hidden>{{ Form::text('t', $t, array('class' => 'form-control')) }}</p>
             <p hidden>{{ Form::text('r', $r, array('class' => 'form-control')) }}</p>
-            <p hidden>{{ Form::text('p', $r, array('class' => 'form-control')) }}</p>
-            {{ Form::submit('Title', array('class' => 'btn btn-default btn-lg')) }}
+            <p hidden>{{ Form::text('p', $p, array('class' => 'form-control')) }}</p>
+            <p hidden>{{ Form::text('d', $d, array('class' => 'form-control')) }}</p>
+            @if ($t === 1)
+                {{ Form::submit('Title', array('class' => 'btn btn-default btn-lg btn-block active')) }}
+                <span class="btn-group dropup"><span class="caret"></span></span>
+            @elseif ($t === 0)
+                {{ Form::submit('Title', array('class' => 'btn btn-default btn-lg btn-block active')) }}
+                <span class="btn-group dropdown"><span class="caret"></span></span>
+            @else
+                {{ Form::submit('Title', array('class' => 'btn btn-default btn-lg btn-block')) }}
+            @endif
             {{ Form::close() }}
             {{--<a href="{{ action('HomeController@orderByTitle', $query) }}" class="btn btn-default btn-lg">Title</a>--}}
         </div>
         <div class="col-xs-2">
-            {{--{{ Form::open(array('route' => 'byPublisher')) }}--}}
-            {{--<p hidden>{{ Form::text('t', $t, array('class' => 'form-control')) }}</p>--}}
-            {{--<p hidden>{{ Form::text('r', $r, array('class' => 'form-control')) }}</p>--}}
-            {{--<p hidden>{{ Form::text('p', $r, array('class' => 'form-control')) }}</p>--}}
-            {{--{{ Form::submit('Publisher', array('class' => 'btn btn-default btn-lg')) }}--}}
-            {{--{{ Form::close() }}--}}
-            <a href="{{ action('HomeController@home') }}" class="btn btn-default btn-lg">Publisher</a>
+            {{ Form::open(array('route' => 'byPublisher')) }}
+            <p hidden>{{ Form::text('t', $t, array('class' => 'form-control')) }}</p>
+            <p hidden>{{ Form::text('r', $r, array('class' => 'form-control')) }}</p>
+            <p hidden>{{ Form::text('p', $p, array('class' => 'form-control')) }}</p>
+            <p hidden>{{ Form::text('d', $d, array('class' => 'form-control')) }}</p>
+            @if ($p === 1)
+                {{ Form::submit('Publisher', array('class' => 'btn btn-default btn-lg btn-block active')) }}
+                <span class="btn-group dropup"><span class="caret"></span></span>
+            @elseif ($p === 0)
+                {{ Form::submit('Publisher', array('class' => 'btn btn-default btn-lg btn-block active')) }}
+                <span class="btn-group dropdown"><span class="caret"></span></span>
+            @else
+                {{ Form::submit('Publisher', array('class' => 'btn btn-default btn-lg btn-block')) }}
+            @endif
+            {{ Form::close() }}
+            {{--<a href="{{ action('HomeController@home') }}" class="btn btn-default btn-lg">Publisher</a>--}}
         </div>
         <div class="col-xs-2">
-            <a href="{{ action('HomeController@home') }}" class="btn btn-default btn-lg">Developer</a>
+            {{ Form::open(array('route' => 'byDeveloper')) }}
+            <p hidden>{{ Form::text('t', $t, array('class' => 'form-control')) }}</p>
+            <p hidden>{{ Form::text('r', $r, array('class' => 'form-control')) }}</p>
+            <p hidden>{{ Form::text('p', $p, array('class' => 'form-control')) }}</p>
+            <p hidden>{{ Form::text('d', $d, array('class' => 'form-control')) }}</p>
+            @if ($d === 1)
+                {{ Form::submit('Developer', array('class' => 'btn btn-default btn-lg btn-block active')) }}
+                <span class="btn-group dropup"><span class="caret"></span></span>
+            @elseif ($d === 0)
+                {{ Form::submit('Developer', array('class' => 'btn btn-default btn-lg btn-block active')) }}
+                <span class="btn-group dropdown"><span class="caret"></span></span>
+            @else
+                {{ Form::submit('Developer', array('class' => 'btn btn-default btn-lg btn-block')) }}
+            @endif
+            {{ Form::close() }}
+            {{--<a href="{{ action('HomeController@home') }}" class="btn btn-default btn-lg">Developer</a>--}}
         </div>
         <div class="col-xs-2">
             {{ Form::open(array('route' => 'byRelease')) }}
             <p hidden>{{ Form::text('t', $t, array('class' => 'form-control')) }}</p>
             <p hidden>{{ Form::text('r', $r, array('class' => 'form-control')) }}</p>
-            <p hidden>{{ Form::text('p', $r, array('class' => 'form-control')) }}</p>
-            {{ Form::submit('Release Date', array('class' => 'btn btn-default btn-lg')) }}
+            <p hidden>{{ Form::text('p', $p, array('class' => 'form-control')) }}</p>
+            <p hidden>{{ Form::text('d', $d, array('class' => 'form-control')) }}</p>
+            @if ($r === 1)
+                {{ Form::submit('Release', array('class' => 'btn btn-default btn-lg btn-block active')) }}
+                <span class="btn-group dropup"><span class="caret"></span></span>
+            @elseif ($r === 0)
+                {{ Form::submit('Release', array('class' => 'btn btn-default btn-lg btn-block active')) }}
+                <span class="btn-group dropdown"><span class="caret"></span></span>
+            @else
+                {{ Form::submit('Release', array('class' => 'btn btn-default btn-lg btn-block')) }}
+            @endif
             {{ Form::close() }}
             {{--<a href="{{ action('HomeController@home') }}" class="btn btn-default btn-lg">Release date</a>--}}
         </div>
